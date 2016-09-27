9eb6d689e84df0a82054d3e9c9a3e595ab45dd27
Xeaphii <muhammadzeeshan020@gmail.com>
2016-09-27 09:13:40 +0500

    Change in the detail activity

--- app/src/main/java/com/zeeshan/pseudo/popularmovies/MainActivity.java
+++ app/src/main/java/com/zeeshan/pseudo/popularmovies/MainActivity.java
@@ -2,20 +2,9 @@ package com.zeeshan.pseudo.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.gridview);
         gridView.setAdapter(new MyAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // DO something
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/zeeshan/pseudo/popularmovies/MyAdapter.java
+++ app/src/main/java/com/zeeshan/pseudo/popularmovies/MyAdapter.java
@@ -27,7 +27,7 @@ import java.util.List;
 /**
  * Created by Pseudo on 9/26/2016.
  */
-  class MyAdapter extends BaseAdapter {
+class MyAdapter extends BaseAdapter {
     private final List<Item> mItems = new ArrayList<Item>();
     private final LayoutInflater mInflater;
     private Context mContext;
@@ -59,9 +59,13 @@ import java.util.List;
                                 JSONObject explrObject = jsonArray.getJSONObject(i);
                                 String title = explrObject.getString("title");
                                 String poster_path = explrObject.getString("poster_path");
+                                String original_title = explrObject.getString("original_title");
+                                String release_date = explrObject.getString("release_date");
+                                String overview = explrObject.getString("overview");
+                                String vote_average = explrObject.getString("vote_average");
 
-                                mItems.add(new Item(title,   "http://image.tmdb.org/t/p/w185//"+    poster_path));
-                                //Toast.makeText(mContext, "http://image.tmdb.org/t/p/w185//"+    poster_path, Toast.LENGTH_LONG).show();
+                                mItems.add(new Item(title, "http://image.tmdb.org/t/p/w185//" + poster_path, original_title, overview, vote_average, release_date));
+
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -72,12 +76,11 @@ import java.util.List;
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(mContext,"Failed to Load data",Toast.LENGTH_LONG).show();
+                Toast.makeText(mContext, "Failed to Load data", Toast.LENGTH_LONG).show();
             }
         });
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
-
 
 
     }
@@ -94,7 +97,7 @@ import java.util.List;
 
     @Override
     public long getItemId(int i) {
-        return  Long.valueOf(i);
+        return Long.valueOf(i);
     }
 
     @Override
@@ -113,24 +116,31 @@ import java.util.List;
         name = (TextView) v.getTag(R.id.text);
 
 
-
         Item item = getItem(i);
 
         //picture.setImageResource(item.drawableId);
         Picasso.with(mContext).load(item.url).into(picture);
 
-        name.setText(item.name);
+        name.setText(item.title);
 
         return v;
     }
 
     private static class Item {
-        public final String name;
+        public final String title;
         public final String url;
+        public final String orig_title;
+        public final String movie_plot;
+        public final String movie_rating;
+        public final String release_date;
 
-        Item(String name, String URL) {
-            this.name = name;
+        Item(String Title, String URL, String Orig_title, String Movie_plot, String Movie_rating, String Release_date) {
+            this.title = Title;
             this.url = URL;
+            this.orig_title = Orig_title;
+            this.movie_plot = Movie_plot;
+            this.movie_rating = Movie_rating;
+            this.release_date = Release_date;
         }
     }
 }
